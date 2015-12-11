bd80f0ac348d463257f2a9b2682d2b0afff03066
Amrit <music.demand01@gmail.com>
2015-12-12 00:47:06 +0545

    some info text on homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,9 @@
 @extends('layout.master')
+
+@section('content')
+    <div class="row">
+        <div class="alert alert-info">
+            Welcome to Client Manager.Navigate to Client menu.
+        </div>
+    </div>
+@endsection
